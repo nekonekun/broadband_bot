65b26364567a57f510e123b331dc8449f57cb429
nekone <nekonekun@gmail.com>
2023-02-05 20:32:01 +0600

    fix(bot): add user full-name to logging middleware

--- stp_bot/middlewares/log.py
+++ stp_bot/middlewares/log.py
@@ -13,9 +13,10 @@ class LoggingMiddleware(BaseMiddleware):
     ) -> Any:
         logger = logging.getLogger('aiogram.middlewares')
         username = event.from_user.username
+        full_name = event.from_user.full_name
         user_id = event.from_user.id
         chat_id = event.chat.id
-        logger.info(f'New message from '
-                    f'@{username} ({user_id}) '
+        logger.info(f'New message from user @{username} '
+                    f'({full_name}, {user_id}) '
                     f'in chat {chat_id}')
         return await handler(event, data)
